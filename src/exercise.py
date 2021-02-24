47a3ddd59ddb15f793cdae7d67e9aa7d62f5aa03
MrSullivan <rsullivan@stcadocs.com>
2021-02-24 23:41:19 +0000

    variable added for two numbers. print commands for the 4 operations.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    first_number = int(input("Give the first number:"))
+    second_number = int(input("Give the second number:"))
+    print(str(first_number) + " + " + str(second_number) + " = " + str(first_number+second_number))
+    print(str(first_number) + " - " + str(second_number) + " = " + str(first_number - second_number))
+    print(str(first_number) + " * " + str(second_number) + " = " + str(first_number*second_number))
+    print(str(first_number) + " / " + str(second_number) + " = " + str(first_number/second_number))
+
 
 if __name__ == '__main__':
     main()
